d62f919d6911234fb4955d1fd77bd7b4ef142679
lhogan3 <lhogan3@uvm.edu>
2018-04-11 13:47:07 -0400

    liams test

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Austin Viveiros" << std::endl;
     std::cout << "Max Peck" << std::endl;
+    std::cout << "waz up liams test" << std::endl;
     return 0;
 }
